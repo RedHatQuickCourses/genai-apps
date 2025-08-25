bae1aceb2df0ae93561a5a147eb4e4d2b934bbe1
Ravi Srinivasan <rsriniva@redhat.com>
2025-08-25 12:06:29 +0530

    added stacked validators lab and cleaned up requirements.txt

---
+++ guardrails/guardrails-ai/gai-basic.py
@@ -0,0 +1,71 @@
+import sys
+import warnings
+import logging
+
+# Ignore UserWarning type warnings that pollute the console
+warnings.filterwarnings("ignore", category=UserWarning)
+
+# Set presidio logger to ERROR level before importing anything that uses it
+logging.getLogger('presidio-analyzer').setLevel(logging.ERROR)
+
+# Import Guard and Validator
+from guardrails import Guard, install
+
+install(
+    "hub://guardrails/detect_pii",
+    install_local_models=True,
+    quiet=False
+)
+
+
+# Import Guard and Validator
+from guardrails.hub import DetectPII
+from guardrails import Guard
+from termcolor import colored, cprint
+
+# Setup Guard
+guard = Guard().use(
+    DetectPII, ["EMAIL_ADDRESS", "PHONE_NUMBER", "US_SSN"], "exception"
+)
+
+def valid_response():
+    guard.validate("Good morning! is your email ID john dot doe at example.com?")
+    print(colored('\nvalid_response(): Response from LLM is valid...', 'green'))
+
+def invalid_response1():
+    try:
+        guard.validate(
+        "Hi John Doe! Can you please verify your email ID john.doe@example.com"
+    )
+    except Exception as e: 
+        msg = str(e)
+        print(colored('\ninvalid_response1(): Response from LLM is invalid\n', 'red'))
+        print(colored(f"{msg}", 'red', attrs=['bold']))
+
+
+def invalid_response2():
+    try:
+        guard.validate(
+        "Hi John Doe! Can you please verify your if your social security number is 615-49-2540"
+    )
+    except Exception as e:
+        msg = str(e)
+        print(colored('\ninvalid_response2(): Response from LLM is invalid\n', 'red'))
+        cprint(f"{msg}", 'red', attrs=['bold'])
+
+def invalid_response3():
+    try:
+        guard.validate(
+        "You can contact John Doe at +1 408-447-4433"
+    )
+    except Exception as e:
+        msg = str(e)
+        print(colored('\ninvalid_response3(): Response from LLM is invalid\n', 'red'))
+        cprint(f"{msg}", 'red', attrs=['bold'])
+
+
+if __name__ == "__main__":
+    valid_response()
+    invalid_response1()
+    invalid_response2()
+    invalid_response3()
---
+++ guardrails/guardrails-ai/gai-stacked.py
@@ -0,0 +1,106 @@
+import sys
+import warnings
+import logging
+
+# Ignore UserWarning type warnings that pollute the console
+warnings.filterwarnings("ignore", category=UserWarning)
+
+# Import Guard and Validator
+from guardrails import Guard, install
+
+install(
+    "hub://guardrails/gibberish_text",
+    install_local_models=True,
+    quiet=False
+)
+
+install(
+    "hub://guardrails/profanity_free",
+    install_local_models=True,
+    quiet=False
+)
+
+install(
+    "hub://guardrails/competitor_check",
+    install_local_models=True,
+    quiet=False
+)
+
+from guardrails.hub import ProfanityFree, GibberishText, CompetitorCheck
+from guardrails.errors import ValidationError
+from termcolor import colored
+
+# Setup Guard
+guard = Guard().use_many(
+    ProfanityFree(),
+    CompetitorCheck(competitors=["Microsoft", "Oracle"]),
+    GibberishText(threshold=0.5, validation_method="sentence", on_fail="exception")
+)
+
+def test_valid_response():
+
+    llm_response = """
+            This is a valid sentence.
+            """
+
+    try:
+        guard.validate(
+            llm_output=llm_response,
+        )
+        print(colored('\ntest_valid_response(): Response from LLM is valid...', 'green'))
+    except ValidationError as e:
+        print(e)
+
+def test_profanity_check():
+
+    llm_response = """
+            Goddamn you! Go to Hell!
+            """
+
+    try:
+        guard.validate(
+            llm_output=llm_response,
+        )
+        
+    except ValidationError as e:
+        msg = str(e)
+        print(colored('\ntest_profanity_check(): Response from LLM is invalid\n', 'red'))
+        print(colored(f"{msg}", 'red', attrs=['bold']))
+
+def test_competitor_check():
+
+    llm_response = """
+            MICROSOFT stock is at an all time high.
+            """
+
+    try:
+        guard.validate(
+            llm_output=llm_response,
+        )
+        
+    except ValidationError as e:
+        msg = str(e)
+        print(colored('\ntest_competitor_check(): Response from LLM is invalid\n', 'red'))
+        print(colored(f"{msg}", 'red', attrs=['bold']))
+
+def test_gibberish_sentence():
+
+    llm_response = """
+            FLoop goop loop doop ba da bing. The previous sentence makes no sense.
+            """
+
+    try:
+        guard.validate(
+            llm_output=llm_response,
+        )
+        
+    except ValidationError as e:
+        msg = str(e)
+        print(colored('\ntest_competitor_check(): Response from LLM is invalid\n', 'red'))
+        print(colored(f"{msg}", 'red', attrs=['bold']))
+
+if __name__ == "__main__":
+    test_valid_response()
+    test_profanity_check()
+    test_competitor_check()
+    test_gibberish_sentence()
